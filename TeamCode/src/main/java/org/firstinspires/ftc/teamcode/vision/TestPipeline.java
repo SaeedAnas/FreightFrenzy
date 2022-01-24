ba730e53638e1c9a23b729cadcd13eed999295e8
Kalkulator413 <kalkin413@gmail.com>
2022-01-23 17:25:39 -0800

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/TestPipeline.java
@@ -1,16 +1,18 @@
 package org.firstinspires.ftc.teamcode.vision;
 
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class TestPipeline extends OpenCvPipeline {
 
-    @Override
-    public Mat processFrame(Mat input)
-    {
-        return input;
+        // Notice this is declared as an instance variable (and re-used), not a local variable
+        Mat grey = new Mat();
+
+        @Override
+        public Mat processFrame(Mat input)
+        {
+            Imgproc.cvtColor(input, grey, Imgproc.COLOR_RGB2GRAY);
+            return grey;
+        }
     }
-
-}
-
-//hi
